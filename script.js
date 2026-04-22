5b2978c63ca0121950597bebf7924ad244a5bd51
Pramod Kumar Rana <ranapramod663@gmail.com>
2026-04-23 00:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) { 
+  if (arr.length <= 1) return 0;
+
+  let cost = 0;
+
+  // Sort initially
+  arr.sort((a, b) => a - b);
+
+  while (arr.length > 1) {
+    // Take two smallest
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let sum = first + second;
+    cost += sum;
+
+    // Insert back and keep sorted
+    arr.push(sum);
+    arr.sort((a, b) => a - b);
+  }
+
+  return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
